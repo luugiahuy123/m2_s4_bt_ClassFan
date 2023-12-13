a3f4eae0064157c94b1fe2952de17d0ba498e489
luugiahuy123 <luugiahuyqn@gmail.com>
2023-12-13 08:57:50 +0700

    bai tap

---
+++ Fan.java
@@ -0,0 +1,33 @@
+package ss4.Fan;
+
+public class Fan {
+    final static int SLOW =1;
+    final static int MEDIUM =2;
+    final static int FAST=3;
+    int speed;
+    boolean on;
+    double radius;
+    String color;
+    public Fan() {
+        this.speed= SLOW;
+        this.on = false;
+        this.radius = 5;
+        this.color = "blue";
+    }
+    public Fan( int speed , boolean on , double radius , String color ){
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Fan{" +
+                "speed=" + speed +
+                ", on=" + on +
+                ", radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ main.java
@@ -0,0 +1,8 @@
+package ss4.Fan;
+
+public class main {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(Fan.FAST , true , 10 , "yellow" );
+        System.out.println(fan1);
+    }
+}
